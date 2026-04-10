31779ea842e278fdcf337e3456c392e5bc69f5b5
Juanma0929 <jumara0929@gmail.com>
2026-04-09 21:43:22 -0500

    Using TDD method for the case logs feature

--- cases/tests.py
+++ cases/tests.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.apps import apps
 
 from cases.models import Case, Category, Subclinic
-from cases.services import create_case
+from cases.services import create_case, create_case_log, get_case_logs
 from users.services import assign_role
 
 User = apps.get_model(settings.AUTH_USER_MODEL)
@@ -81,3 +81,67 @@ class CreateCaseTest(TestCase):
         case = create_case(self.admin, 'description', self.category, self.subclinic)
         self.assertIsNotNone(case.pk)
         self.assertTrue(Case.objects.filter(pk=case.pk).exists())
+
+
+class CaseLogServiceTest(TestCase):
+    """Tests for case log creation and retrieval rules."""
+
+    def setUp(self):
+        self.subclinic = Subclinic.objects.create(name='family')
+        self.category, _ = Category.objects.get_or_create(name='civil')
+
+        self.admin = User.objects.create_user(username='admin_logs', password='pass')
+        assign_role(self.admin, 'admin')
+
+        self.advisor = User.objects.create_user(username='advisor_logs', password='pass')
+        assign_role(self.advisor, 'advisor')
+
+        self.professor = User.objects.create_user(username='professor_logs', password='pass')
+        assign_role(self.professor, 'professor')
+
+        self.student = User.objects.create_user(username='student_logs', password='pass')
+        assign_role(self.student, 'student')
+
+        self.beneficiary = User.objects.create_user(username='beneficiary_logs', password='pass')
+        assign_role(self.beneficiary, 'beneficiary')
+
+        self.other_student = User.objects.create_user(username='other_student_logs', password='pass')
+        assign_role(self.other_student, 'student')
+
+        self.case = create_case(self.student, 'Case for logs', self.category, self.subclinic)
+        self.case.users.add(self.professor)
+
+    def test_valid_roles_can_create_case_log(self):
+        valid_users = [self.student, self.professor, self.advisor, self.admin]
+
+        for index, user in enumerate(valid_users, start=1):
+            log = create_case_log(user, self.case, f'Log from {user.username} #{index}')
+            self.assertEqual(log.user, user)
+            self.assertEqual(log.case, self.case)
+
+    def test_beneficiary_cannot_create_case_log(self):
+        self.case.users.add(self.beneficiary)
+
+        with self.assertRaises(PermissionError):
+            create_case_log(self.beneficiary, self.case, 'I should not be able to log')
+
+    def test_unassigned_non_privileged_user_cannot_create_case_log(self):
+        with self.assertRaises(PermissionError):
+            create_case_log(self.other_student, self.case, 'Not assigned to case')
+
+    def test_log_is_associated_with_case(self):
+        log = create_case_log(self.student, self.case, 'Progress update')
+        self.assertTrue(self.case.logs.filter(pk=log.pk).exists())
+
+    def test_logs_are_returned_in_chronological_order(self):
+        first = create_case_log(self.professor, self.case, 'First feedback')
+        second = create_case_log(self.student, self.case, 'Second update')
+        third = create_case_log(self.advisor, self.case, 'Third note')
+
+        logs = list(get_case_logs(self.case))
+        self.assertGreaterEqual(len(logs), 3)
+        self.assertEqual([logs[-3].pk, logs[-2].pk, logs[-1].pk], [first.pk, second.pk, third.pk])
+
+    def test_log_content_cannot_be_empty(self):
+        with self.assertRaises(ValueError):
+            create_case_log(self.student, self.case, '   ')
