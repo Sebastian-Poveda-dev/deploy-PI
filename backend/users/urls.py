dbe71221521c32dae14854f30450239ee3edb523
Juanma0929 <jumara0929@gmail.com>
2026-04-12 20:32:54 -0500

    Fix

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-<<<<<<< HEAD
 from .views import (
+    beneficiaries_view,
     login_view,
     me_view,
     professors_view,
@@ -20,11 +20,7 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('me/', me_view, name='me'),
     path('professors/', professors_view, name='professors'),
-<<<<<<< HEAD
     path('', UserManagementListCreateView.as_view(), name='user-list-create'),
     path('<int:pk>/', UserManagementDetailView.as_view(), name='user-detail'),
-]
-=======
     path('beneficiaries/', beneficiaries_view, name='beneficiaries'),
 ]
->>>>>>> dev
